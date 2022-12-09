547574f3535dbaf23a6f46a9542f4bf537377339
humamaboalraja <humamaboalraja@gmail.com>
2022-12-09 20:39:09 +0100

    chore: release v0.2.1

--- src/config/app.ts
+++ src/config/app.ts
@@ -4,7 +4,7 @@ dotenv.config();
 
 const config = {
   package_name: process.env.npm_package_name || "cli-ai",
-  package_version: process.env.npm_package_version || "0.1.3",
+  package_version: process.env.npm_package_version || "0.2.1",
   openai_api_token: process.env.openai_api_token,
   repo_url:
     process.env.npm_package_bugs_url ||
