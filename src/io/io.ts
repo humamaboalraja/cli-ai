dba709858ea3e784e1fcb51c9afa84a97a3ca6c4
humamaboalraja <humamaboalraja@gmail.com>
2022-12-09 19:53:05 +0100

    feat: Add `max token` option

--- src/io/io.ts
+++ src/io/io.ts
@@ -1,13 +1,13 @@
 import { program } from "commander";
-import { config } from "../config/app";
+import { config } from "../config";
 import { Cli } from "./cli/cli";
 
 const cli: Cli = new Cli(program);
 
 const cli_options = (): Cli => {
   try {
-    cli.name(config.cli_name);
-    cli.version(config.app_version, "-v");
+    cli.name(config.package_name);
+    cli.version(config.package_version, "-v, --version", "CLI-AI's version ");
     cli.description("A CLI for interacting with OpenAI's GPT3 API");
 
     cli.register_option("-ask, --question <value>", "Add your prompt");
@@ -15,14 +15,14 @@ const cli_options = (): Cli => {
       "-m, --model <value>",
       "Add your preferred training model e.g. 'text-davinci-003'"
     );
-    cli.register_option("-c, --chat [type]", 'Enable chatting mode "Active"');
+    cli.register_option("-c, --chat [type]", 'Enable chatting mode "active"');
     cli.register_option(
       "-t, --temperature <value>",
       'Set temperature e.g. default "0.9"'
     );
     cli.register_option(
       "-mt, --max-tokens <value>",
-      'Set max tokens e.g. default "300"'
+      "Set words count. default 300"
     );
   } catch (e) {
     console.log(`💥 ${e}\n`);
